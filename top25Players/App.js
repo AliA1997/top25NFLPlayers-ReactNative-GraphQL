6f0a533029f1cf270048e97e9f2c45b532d3bf0e
Ali Alhaddad <ali.alhaddad@kikoda.com>
2019-01-21 20:53:30 -0500

    Finished First Part of TUtorial

--- top25Players/App.js
+++ top25Players/App.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+//import your Platform for conditionally change the url based if it is on ios or android. 
+//Since on android you would have set the url to your emulator, while on ios it would be set to localhost.
+import { Platform } from 'react-native';
 //import your ApolloProvider from react-apollo to wrap your app.
 import { ApolloProvider} from 'react-apollo'; 
 //import ApolloClient, InMemoryCache, and HttpLink to define your client to cnnect to your graphql server.//#endregion
@@ -11,7 +14,11 @@ const client = new ApolloClient({
   //Assign to your cache property a instance of a InMemoryCache
   cache: new InMemoryCache(),
   //Assign your link with a new instance of a HttpLink linking to your graphql server.
-  link: new HttpLink({uri: 'http://localhost:4000/graphql'})
+  link: new HttpLink({uri: Platform.select({
+      ios: 'http://localhost:4000/graphql',
+      android: 'http://10.0.2.2:4000/graphql'
+    })
+  })
 })
 
 
